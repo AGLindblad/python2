ca1f75c4668479095d3c66012b250ab8cb80c44e
Anders Lindblad <agl1983@yahoo.com>
2021-05-25 09:38:47 +0300

    korjattu versio

--- muotti.py
+++ muotti.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
-app = Flask(__name__)=
+app = Flask(__name__)
 
 @app.route("/")
 def index():
-  return reder_template("base.html")
+  return render_template("base.html")
 
 if __name__== "__main__":
+  app.run()
 
